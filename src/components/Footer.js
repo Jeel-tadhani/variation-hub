9dc9b2f0a6a6dc861e029aadb280bb24539aea09
Yashbhimani623 <bhimaniyash623@gmail.com>
2024-05-10 14:24:57 +0530

    Home Page Responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,26 +23,28 @@ function Footer() {
           <p className="p-footer">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncove</p>
         </Col>
 
-        <Col md="3" className="footer-copywright footer-header">
-          <strong>Our Services</strong>
-          <br />
-          <br />
-          <Link as={Link} to="/">Home</Link>
-          <br />
-          <Link as={Link} to="/our-services">Services</Link>
-          <br />
-          <Link as={Link} to="/project">Projects</Link>
-          <br />
-          <Link as={Link} to="/about">About</Link>
-          <br />
-          <Link as={Link} to="/contact-us">ContactUs</Link>
+        <Col md="3" className="footer-copywright footer-header links-services">
+          <div className="footer-body quick-links">
+            <strong className="title">Quick Links</strong>
+            <Link className="body-data" as={Link} to="/">Home</Link>
+            <Link className="body-data" as={Link} to="/our-services">Services</Link>
+            <Link className="body-data" as={Link} to="/project">Projects</Link>
+            <Link className="body-data" as={Link} to="/about">About</Link>
+            <Link className="body-data" as={Link} to="/contact-us">ContactUs</Link>
+          </div>
+          <div className="footer-body">
+            <strong className="title">Our Services</strong>
+            <Link className="body-data" as={Link} to="/web-services">Web Services</Link>
+            <Link className="body-data" as={Link} to="/app-services">App Services</Link>
+            <Link className="body-data" as={Link} to="/blockchain-services">Blockchain Services</Link>
+            {/* <Link className="body-data" as={Link} to="/about">About</Link> */}
+            {/* <Link className="body-data" as={Link} to="/contact-us">ContactUs</Link> */}
+          </div>
         </Col>
 
         <Col md="3" className="footer-copywright footer-header">
-          <strong>Contact Us</strong>
-          <br />
-          <br />
-          <div>
+          <strong className="title">Contact Us</strong>
+          <div className="body-data">
             <a style={{ fontFamily: "math" }} href="tel:9773203735">+91 9773203735</a>
             <br />
             <a style={{ fontFamily: "math" }} href="mailto:service@variationhub.com" >service@variationhub.com</a>
@@ -58,56 +60,53 @@ function Footer() {
         </Col>
 
         <Col md="3" className="footer-header">
-          <strong>Social Media</strong>
-          <br />
+          <strong className="title">Social Media</strong>
           <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="/"
-                className="footer-svg"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <br />
+            <div className="footer-body f-icons">
+              <li className="social-icons">
+                <a
+                  href="/"
+                  className="footer-svg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
 
-            <li className="social-icons">
-              <a
-                href="https://wa.me/9773203735"
-                className="footer-svg"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiOutlineWhatsApp />
-              </a>
-            </li>
-            <br />
+              <li className="social-icons">
+                <a
+                  href="https://wa.me/9773203735"
+                  className="footer-svg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiOutlineWhatsApp />
+                </a>
+              </li>
 
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/company/variation-hub/"
-                className="footer-svg"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <br />
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/company/variation-hub/"
+                  className="footer-svg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
 
-            <li className="social-icons">
-              <a
-                href="/"
-                className="footer-svg"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-            <br />
+              <li className="social-icons">
+                <a
+                  href="/"
+                  className="footer-svg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </div>
 
           </ul>
         </Col>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -17,7 +17,7 @@ function Home2() {
           OUR SERVICES
         </h1>
 
-        <p style={{ color: "white", width: "50%" }}>
+        <p style={{ color: "white", width: "100%", textAlign: "justify"}}>
           Here are a few services I've worked on recently.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
         </p>
 
@@ -30,28 +30,31 @@ function Home2() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ServicesCards
+              className="card-img-top"
               imgPath={website}
               isBlog={false}
               title="Web Services"
               description="Elevate your online presence with our cutting-edge web application services. We design responsive, user-friendly interfaces that seamlessly integrate with the latest technologies. Experience enhanced functionality, scalability, and a visually appealing digital platform that engages and captivates your audience."
               rdLink="/web-services"
-              // demoLink="https://chatify-49.web.app/"
-              />
+            // demoLink="https://chatify-49.web.app/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
             <ServicesCards
+              className="card-img-top"
               imgPath={mobileApplication}
               isBlog={false}
               title="App Services"
               description="Revolutionize your reach with our mobile application services. We craft intuitive, high-performance apps that resonate with your audience. Whether iOS or Android, our mobile solutions blend aesthetics with functionality, providing a delightful user experience. Stay ahead in the mobile landscape, harnessing the power of innovation for your business success."
               rdLink="/app-services"
-              // demoLink="https://editor.soumya-jit.tech/"
-              />
+            // demoLink="https://editor.soumya-jit.tech/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
             <ServicesCards
+              className="card-img-top"
               imgPath={iotDevelopment}
               isBlog={false}
               title="Blockchain Services"
